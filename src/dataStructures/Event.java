b074b5e6d9eaa7b473190a8e040badf744b1c2a6
fhausken <fredrik280893@gmail.com>
2015-02-25 17:47:29 +0100

    Event

--- src/dataStructures/Event.java
+++ src/dataStructures/Event.java
@@ -1,5 +1,18 @@
 package dataStructures;
 
+import java.util.ArrayList;
+import java.util.Date;
+
 public class Event {
+	
+	private ArrayList<User> participants;
+	private ArrayList<Group> groups;
+	public String name;
+	public String description;
+	public Date date;
+	public Double duration;
+	public Room room;
+	public String uid;
+	
 
 }
