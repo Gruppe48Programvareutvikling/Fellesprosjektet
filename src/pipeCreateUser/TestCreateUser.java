01b9995211be9991e9b6f446dee7708afd28f259
Jakob Deraas Grimsgaard <jakobgrimsgaard@gmail.com>
2015-02-27 12:47:43 +0100

    Unit testing experimenting

---
+++ src/pipeCreateUser/TestCreateUser.java
@@ -0,0 +1,23 @@
+package pipeCreateUser;
+
+import static org.junit.Assert.*;
+import junit.framework.TestCase;
+import org.mockito.*;
+
+import org.junit.Test;
+
+public class TestCreateUser extends TestCase{
+
+	
+	
+	
+	
+	
+	
+	
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
