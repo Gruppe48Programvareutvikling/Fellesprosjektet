704d4137dd30ce4772178debf0f3c928d5e64133
MaSommer <martisom@stud.ntnu.no>
2015-03-02 13:13:02 +0100

    Working with Notification

--- src/pipeGetNotifications/UserGetNotifications.java
+++ src/pipeGetNotifications/UserGetNotifications.java
@@ -1,9 +1,14 @@
 package pipeGetNotifications;
 
 import mainControlStructure.ControllerInterface;
+import serverReturnTypes.ServerAvailabilityResult;
 import superClasses.SuperUser;
 
 public class UserGetNotifications extends SuperUser {
+	
+	private ServerGetNotifications server = new ServerGetNotifications();
+	private String username = "username";
+
 	
 	public UserGetNotifications(ControllerInterface delegator) {
 		this.delegator = delegator;
@@ -13,7 +18,23 @@ public class UserGetNotifications extends SuperUser {
 	}
 	
 	public void startRunning() {
-		
+		ServerAvailabilityResult notifications = this.server.getNotifications(username);
+		if (!notifications.isAvailable){
+			if (notifications.didSucceed){
+				this.delegator.delegateIsDone("You have none recent notifications");				
+			}
+			else{
+				this.delegator.delegateIsDone("There was an error creating the user with the message \"" + notifications.errorMessage + "\"");
+			}
+		}
+		else{
+			if (notifications.didSucceed){
+				this.delegator.delegateIsDone("This is all your notifications");
+			}
+			else{
+				this.delegator.delegateIsDone("There was an error creating the user with the message \"" + notifications.errorMessage + "\"");
+			}
+		}
 	}
 	
 	public void sendNextInput(String nextInput) {
