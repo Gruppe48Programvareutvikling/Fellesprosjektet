d08be1cee9bb5018f29103a316a0e78f76fa6584
fhausken <fredrik280893@gmail.com>
2015-03-16 15:32:10 +0100

    lagt til event

---
+++ src/dataStructures/Event.java
@@ -0,0 +1,17 @@
+package dataStructures;
+import java.sql.Timestamp;
+public class Event {
+	
+	public Integer 	 eventId;
+	public String 	 name;
+	public String 	 description;
+	public Timestamp startDate = new Timestamp(0);
+	public Timestamp endDate = new Timestamp(0);
+	public String    privateCalendarName;
+	public String 	 groupCalendarName;
+	public String 	 location;
+	public String 	 creator;
+	public Integer   roomNumber;
+		
+
+}
